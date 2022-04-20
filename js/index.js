c0f597bcf3a9098b6cb9ae3ebeaebe210eb57b61
Duro <davidwright13503@gmail.com>
2022-04-20 19:25:02 -0400

    remove console log

--- js/index.js
+++ js/index.js
@@ -2,8 +2,6 @@
   const data = await axios.get('http://suta-production-5093.up.railway.app:80');
   const { servers, users } = data.data;
 
-  console.log({ servers, users })
-
   const serversDOM = document.getElementById('servers');
   const usersDOM = document.getElementById('users');
   
