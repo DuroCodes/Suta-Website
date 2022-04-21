afbd0017da58e42aa7084d061b19924c205fa63d
Duro <davidwright13503@gmail.com>
2022-04-21 07:45:01 -0400

    Update 🚀

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 (async () => {
-  const data = await axios.get('http://suta-production-5093.up.railway.app:80');
-  const { servers, users } = data.data;
+  const { data } = await axios.get('http://suta-production-5093.up.railway.app:80');
+  const { servers, users } = data;
 
   const serversDOM = document.getElementById('servers');
   const usersDOM = document.getElementById('users');
-  
+
   serversDOM.innerText = servers;
   usersDOM.innerText = users;
 })();
